bce17dd77b3518febb12cc1f23e06b0ed37a569e
DaWitt99 <dwittbrodt99@gmail.com>
2026-05-14 09:29:19 +0200

    Second approach: removed rolls, added back moves, added twice moves, changed input from integers to one-hot encoding, added Residual Blocks, changed reward system, removed pre-training

--- cube.py
+++ cube.py
@@ -1,8 +1,9 @@
 from random import randint
+import tensorflow as tf
 import numpy as np
 
 class Cube:
-    def __init__(self, ds, var=0):
+    def __init__(self, ds, init_moves=0):
         '''
         white == 0
         yellow == 1
@@ -28,27 +29,54 @@ class Cube:
             self.up = [[4] * 3 for i in range(3)]
             self.bottom = [[5] * 3 for i in range(3)]
 
+        self.move_history = []
+        self.state_history = []
+        self.last_layers_done = [0,0,0]
+        self.present_layers_done = [0,0,0]
         self.counter = 0
-        if var == 0:
-            self.make_random_moves(8)
-        elif var == 1:
-            self.move_front_clockwise()
-        elif var == 2:
-            self.move_front_counterclockwise()
-        self.counter = 0
-        self.history = []
+
+        self.make_random_moves(init_moves)
 
     def show(self):
-        print(self.front, self.back, self.left, self.right, self.up, self.bottom, sep='\n')
+        for i in range(self.ds):
+            print("   ",end="")
+            for j in range(self.ds):
+                print(self.up[i][j], end="")
+            print('')
+        for i in range(self.ds):
+            for j in range(self.ds):
+                print(self.left[i][j], end="")
+            for j in range(self.ds):
+                print(self.front[i][j], end="")
+            for j in range(self.ds):
+                print(self.right[i][j], end="")
+            print('')
+        for i in range(self.ds):
+            print("   ",end="")
+            for j in range(self.ds):
+                print(self.bottom[i][j], end="")
+            print('')
+        for i in range(self.ds):
+            print("   ",end="")
+            for j in range(self.ds):
+                print(self.back[i][j], end="")
+            print('')
 
     def get_state(self):
         return np.array([self.front, self.back, self.left, self.right, self.up, self.bottom]).reshape(1,-1)
 
-    def get_history(self):
-        return self.history
+    def get_state_one_hot(self):
+        return np.array((tf.one_hot(self.front, 6),tf.one_hot(self.back, 6),tf.one_hot(self.left, 6),tf.one_hot(self.right, 6),
+                tf.one_hot(self.up, 6),tf.one_hot(self.bottom, 6))).reshape(1,-1)
+
+    def get_move_history(self):
+        return self.move_history
+
+    def get_state_history(self):
+        return self.state_history
 
     def count_layers_done(self):
-        layers_done = 0
+        layers_done = [0,0,0]
         for side in [self.front, self.back, self.left, self.right, self.up, self.bottom]:
             for i in range(len(side)):
                 layer = 1
@@ -59,19 +87,13 @@ class Cube:
                     else:
                         break
                 if layer == self.ds:
-                    layers_done += 1
+                    layers_done[i] +=1
 
-                layer = 1
-                first = side[0][i]
-                for j in range(1, len(side)):
-                    if side[j][i] == first:
-                        layer += 1
-                    else:
-                        break
-                if layer == self.ds:
-                    layers_done += 1
+        self.last_layers_done = self.present_layers_done
+        self.present_layers_done = layers_done
 
-        return layers_done
+    def get_layers_done(self):
+        return self.last_layers_done, self.present_layers_done
 
     def get_side_incorrect_squares(self, side_name):
         incorrect_squares = 0
@@ -127,23 +149,7 @@ class Cube:
             return total_incorrect_squares/48.0
 
     # every important move on cube
-    # 4 rolls
-    # 10 moves
-
-    # rolling
-    def roll_forward(self):
-        self.front, self.up, self.back, self.bottom = self.bottom, self.front, self.up, self.back
-
-    def roll_backward(self):
-        self.front, self.up, self.back, self.bottom = self.up, self.back, self.bottom, self.front
-
-    def roll_left(self):
-        self.left, self.up, self.right, self.bottom = self.up, self.right, self.bottom, self.left
-
-    def roll_right(self):
-        self.right, self.up, self.left, self.bottom = self.up, self.left, self.bottom, self.right
-
-    # moving
+    # 12 moves
     def move_front_clockwise(self):
         if self.ds==2:
             (
@@ -191,8 +197,6 @@ class Cube:
             )
 
     def move_front_counterclockwise(self):
-        # front squares
-
         if self.ds==2:
 
             (
@@ -236,6 +240,10 @@ class Cube:
                 self.bottom[0][2], self.bottom[0][1], self.bottom[0][0],
                 self.left[0][2], self.left[1][2], self.left[2][2],
             )
+
+    def move_front_clockwise_twice(self):
+        self.move_front_clockwise()
+        self.move_front_clockwise()
 
     def move_left_clockwise(self):
         if self.ds==2:
@@ -323,6 +331,10 @@ class Cube:
                 self.bottom[0][2], self.bottom[0][1], self.bottom[0][0],
                 self.back[0][2], self.back[1][2], self.back[2][2],
             )
+
+    def move_left_clockwise_twice(self):
+        self.move_left_clockwise()
+        self.move_left_clockwise()
 
     def move_right_clockwise(self):
         if self.ds==2:
@@ -412,6 +424,10 @@ class Cube:
                 self.bottom[2][0], self.bottom[1][0], self.bottom[0][0],
                 self.front[0][2], self.front[1][2], self.front[2][2],
             )
+
+    def move_right_clockwise_twice(self):
+        self.move_right_clockwise()
+        self.move_right_clockwise()
 
     def move_up_clockwise(self):
         if self.ds==2:
@@ -503,6 +519,10 @@ class Cube:
                 self.left[0][0], self.left[0][1], self.left[0][2],
             )
 
+    def move_up_clockwise_twice(self):
+        self.move_up_clockwise()
+        self.move_up_clockwise()
+
     def move_bottom_clockwise(self):
         if self.ds ==2:
             (
@@ -593,42 +613,149 @@ class Cube:
                 self.left[2][0], self.left[2][1], self.left[2][2],
             )
 
-    def make_move(self, move):
+    def move_bottom_clockwise_twice(self):
+        self.move_bottom_clockwise()
+        self.move_bottom_clockwise()
+
+    def move_back_clockwise(self):
+        if self.ds == 2:
+            (
+                self.back[0][0], self.back[0][1],
+                self.back[1][0], self.back[1][1]) = (
+                self.back[1][0], self.back[0][0],
+                self.back[1][1], self.back[0][1]
+            )
+
+            (
+                self.left[0][0], self.left[1][0],
+                self.up[0][0], self.up[0][1],
+                self.right[0][1], self.right[1][1],
+                self.bottom[1][0], self.bottom[1][1],
+            ) = (
+                self.up[0][1], self.up[0][0],
+                self.right[0][1], self.right[1][1],
+                self.bottom[1][1], self.bottom[1][0],
+                self.left[0][0], self.left[1][0],
+            )
+
+        else:
+            (
+                self.back[0][0], self.back[0][1], self.back[0][2],
+                self.back[1][0], self.back[1][2],
+                self.back[2][0], self.back[2][1], self.back[2][2]) = (
+
+                self.back[2][0], self.back[1][0], self.back[0][0],
+                self.back[2][1], self.back[0][1],
+                self.back[2][2], self.back[1][2], self.back[0][2]
+            )
+
+            (
+                self.left[0][0], self.left[1][0], self.left[2][0],
+                self.up[0][0], self.up[0][1], self.up[0][2],
+                self.right[0][2], self.right[1][2], self.right[2][2],
+                self.bottom[2][0], self.bottom[2][1], self.bottom[2][2],
+            ) = (
+                self.up[0][2], self.up[0][1], self.up[0][0],
+                self.right[0][2], self.right[1][2], self.right[2][2],
+                self.bottom[2][2], self.bottom[2][1], self.bottom[2][0],
+                self.left[0][0], self.left[1][0], self.left[2][0]
+            )
+
+    def move_back_counterclockwise(self):
+        if self.ds == 2:
+
+            (
+                self.back[0][0], self.back[0][1],
+                self.back[1][0], self.back[1][1]) = (
+                self.back[0][1], self.back[1][1],
+                self.back[0][0], self.back[1][0]
+            )
+
+            (
+                self.left[0][0], self.left[1][0],
+                self.up[0][0], self.up[0][1],
+                self.right[0][1], self.right[1][1],
+                self.bottom[1][0], self.bottom[1][1],
+            ) = (
+                self.bottom[1][0], self.bottom[1][1],
+                self.left[1][0], self.left[0][0],
+                self.up[0][0], self.up[0][1],
+                self.right[1][1], self.right[0][1],
+            )
+        else:
+            (
+                self.back[0][0], self.back[0][1], self.back[0][2],
+                self.back[1][0], self.back[1][2],
+                self.back[2][0], self.back[2][1], self.back[2][2]) = (
+
+                self.back[0][2], self.back[1][2], self.back[2][2],
+                self.back[0][1], self.back[2][1],
+                self.back[0][0], self.back[1][0], self.back[2][0]
+            )
+
+            (
+                self.left[0][0], self.left[1][0], self.left[2][0],
+                self.up[0][0], self.up[0][1], self.up[0][2],
+                self.right[0][2], self.right[1][2], self.right[2][2],
+                self.bottom[2][0], self.bottom[2][1], self.bottom[2][2],
+            ) = (
+                self.bottom[2][0], self.bottom[2][1], self.bottom[2][2],
+                self.left[2][0], self.left[1][0], self.left[0][0],
+                self.up[0][0], self.up[0][1], self.up[0][2],
+                self.right[2][2], self.right[1][2], self.right[0][2],
+            )
+
+    def move_back_clockwise_twice(self):
+        self.move_back_clockwise()
+        self.move_back_clockwise()
+
+    def make_move(self, move, init=False):
         if move == 0:
-            self.roll_forward()
-        elif move == 1:
-            self.roll_backward()
-        elif move == 2:
-            self.roll_left()
-        elif move == 3:
-            self.roll_right()
-        elif move == 4:
             self.move_front_clockwise()
-        elif move == 5:
+        elif move == 1:
             self.move_front_counterclockwise()
-        elif move == 6:
+        elif move == 2:
             self.move_left_clockwise()
-        elif move == 7:
+        elif move == 3:
             self.move_left_counterclockwise()
-        elif move == 8:
+        elif move == 4:
             self.move_right_clockwise()
-        elif move == 9:
+        elif move == 5:
             self.move_right_counterclockwise()
-        elif move == 10:
+        elif move == 6:
             self.move_up_clockwise()
-        elif move == 11:
+        elif move == 7:
             self.move_up_counterclockwise()
-        elif move == 12:
+        elif move == 8:
             self.move_bottom_clockwise()
-        elif move == 13:
+        elif move == 9:
             self.move_bottom_counterclockwise()
+        elif move == 10:
+            self.move_back_clockwise()
+        elif move == 11:
+            self.move_back_counterclockwise()
+        elif move == 12:
+            self.move_front_counterclockwise()
+        elif move == 13:
+            self.move_left_clockwise_twice()
+        elif move == 14:
+            self.move_right_clockwise_twice()
+        elif move == 15:
+            self.move_up_clockwise_twice()
+        elif move == 16:
+            self.move_bottom_clockwise_twice()
+        elif move == 17:
+            self.move_back_clockwise_twice()
 
-        self.history.append(move)
+        if not init:
+            self.move_history.append(move)
+            self.state_history.append(self.get_state_one_hot())
+            self.count_layers_done()
+            self.counter += 1
 
-        self.counter += 1
-        return self.get_state(), self.get_cube_entropy(), self.counter, self.get_cube_entropy()==0, self.counter>=100
+        return self.get_state_one_hot(), self.get_cube_entropy(), self.counter, self.get_cube_entropy()==0, self.counter>=200
 
     def make_random_moves(self, n):
         for i in range(n):
-            rand = randint(0,14)
-            self.make_move(rand)
+            rand = randint(0,12)
+            self.make_move(rand, 1)
--- cube_rl.py
+++ cube_rl.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import numpy as np
 
 class DenseBN(tf.keras.layers.Layer):
-    def __init__(self,units=32, activation='elu', **kwargs):
+    def __init__(self,units=64, activation='elu', **kwargs):
         super().__init__(**kwargs)
         self.units = units
         self.activation = tf.keras.activations.get(activation)
@@ -21,110 +21,137 @@ class DenseBN(tf.keras.layers.Layer):
         base_config = super().get_config()
         return {**base_config, 'units': self.units, 'activation': tf.keras.activations.serialize(self.activation)}
 
+class ResidualBlock(tf.keras.layers.Layer):
+    def __init__(self, units = 128, activation='relu', **kwargs):
+        super().__init__(**kwargs)
+        self.units = units
+        self.activation = tf.keras.activations.get(activation)
+        self.layers = [
+            tf.keras.layers.Dense(units=self.units, kernel_initializer="he_normal"),
+            tf.keras.layers.BatchNormalization(),
+            self.activation,
+            tf.keras.layers.Dense(units=self.units, kernel_initializer="he_normal"),
+            tf.keras.layers.BatchNormalization(),
+        ]
+        self.skip_layers = [
+            tf.keras.layers.Dense(units=self.units, kernel_initializer="he_normal"),
+            tf.keras.layers.BatchNormalization(),
+        ]
+
+    def call(self, inputs):
+        Y = inputs
+        for layer in self.layers:
+            Y=layer(Y)
+
+        Y_skip = inputs
+        for skip_layer in self.skip_layers:
+            Y_skip = skip_layer(Y_skip)
+
+        return self.activation(Y+Y_skip)
+
+    def get_config(self):
+        base_config = super().get_config()
+        return {**base_config, 'units': self.units, 'activation': tf.keras.activations.serialize(self.activation)}
+
 def sample_experiences(batch_size=64):
     indices = np.random.randint(low=0, high=len(experiences_buffer)-1, size=batch_size)
     exps = [experiences_buffer[i] for i in indices]
     return [np.array([experience[field_index] for experience in exps]) for field_index in range(6)]
 
-def epsilon_greedy_policy(state, epsilon=0):
+def epsilon_greedy_policy(model, state, epsilon=0):
     if np.random.random() < epsilon:
         return np.random.randint(0,13)
     else:
-        Q_vals = cube_solver.predict(state)
+        Q_vals = model.predict(state)
         return np.argmax(Q_vals)
 
-def play_one_step(epsilon, cube, counter=-1):
-    state = cube.get_state()
-    init_layers_done = cube.count_layers_done()
-    init_entropy = cube.get_cube_entropy()
-
-    if counter != -1:
-        action = counter
-    else:
-        action = epsilon_greedy_policy(state,epsilon)
+def play_one_step(model, epsilon, cube):
+    state = cube.get_state_one_hot()
+    action = epsilon_greedy_policy(model,state,epsilon)
 
     next_state, cube_entropy, move_counter, done, truncated = cube.make_move(action)
-    layers_done = cube.count_layers_done()
+    layers_done = cube.get_layers_done()
 
-    if counter != -1:
-        reward = 5
-    else:
-        reward = (layers_done - init_layers_done) + (init_entropy - cube_entropy) * 10.0 + done * 100 - truncated * 100
+    layer_reward = 0
+    for i in range(cube.ds):
+        if layers_done[1][i] == 1 and layers_done[0][i] == 0:
+            layer_reward += 5
+
+    no_repeats_rewards = 0
+    for past_state in cube.get_state_history()[:-1]:
+        if np.array_equal(state,past_state):
+            no_repeats_rewards -=1
+
+    reward = no_repeats_rewards + layer_reward + done * 1200 - 1
 
     experiences_buffer.append([state, action, reward, next_state, done, truncated])
     return next_state, reward, done, truncated, cube_entropy, action
 
-def training_step(batch_size=32):
+def training_step(model, batch_size=32):
     exps = sample_experiences(batch_size)
-    states, actions, rewards, next_states = exps
-
+    states, actions, rewards, next_states, dones, truncateds = exps
     next_Q_values = []
     for next in next_states:
-        next_Q_values.append(cube_solver.predict(np.array(next), verbose=0))
+        next_Q_values.append(model.predict(next, verbose=0))
     max_next_Q_values = np.max(next_Q_values, axis=2)
     target_Q_values = rewards.reshape(batch_size,1) + discount * max_next_Q_values
     target_Q_values = target_Q_values.reshape(-1,1)
-    mask = tf.one_hot(actions, 14)
+    mask = tf.one_hot(actions, 18)
 
     with tf.GradientTape() as tape:
-        all_Q_values = [cube_solver(state) for state in states]
+        all_Q_values = [model(state) for state in states]
         Q_values = tf.reduce_max(tf.reduce_sum(all_Q_values*mask, axis=2, keepdims=True),axis=1)
         loss = tf.reduce_mean(tf.keras.losses.mse(target_Q_values, Q_values))
+        print(loss)
 
-    gradients = tape.gradient(loss, cube_solver.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, cube_solver.trainable_variables))
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-def training_loop(episodes):
+def training_loop(model, episodes):
     for episode in range(episodes):
-        cube = Cube(3,-1)
+        cube = Cube(3,1)
 
-        for step in range(100):
-            epsilon = max(1-episode/50,0.1)
-            next_state,reward,done,truncated,entropy,action = play_one_step(epsilon, cube)
+        for step in range(200):
+            epsilon = max(1-episode/200,0.1)
+            next_state,reward,done,truncated,entropy,action = play_one_step(model,epsilon, cube)
             print("EPISODE", episode, "STEP", step, 'REWARD', reward, 'DONE', done, "ENTROPY", entropy)
 
             if done or truncated:
                 break
 
-        if episode > 20:
-            training_step()
+        if episode > -1:
+            training_step(model)
             cube_solver.save('cubeRL.keras')
 
-# train going backward on cube's moves history made of random moves
-def pre_training_loop(episodes):
+def testing_loop(model, episodes):
     for episode in range(episodes):
-        cube = Cube(3,-1)
-        cube.make_random_moves(episode)
-        history = cube.get_history()
-        counter_history =[]
-
-        for i in range(len(history)):
-            if history[i] % 2 == 0:
-                counter_history.append(history[i]+1)
-            else:
-                counter_history.append(history[i]-1)
-
-        for i in range(len(history)):
-            next_state, reward, done, truncated, entropy, action = play_one_step(0, cube, counter_history.pop())
-            print("EPISODE", episode, "STEP", i, 'REWARD', reward, 'DONE', done, "ENTROPY", entropy)
-
-        if episode > 20:
-            training_step()
-            cube_solver.save('cubeRL.keras')
+        cube = Cube(3,1)
+        for step in range(200):
+            next_state, reward, done, truncated, entropy, action = play_one_step(model, 0, cube)
+            print("TEST EPISODE", episode, "STEP", step, 'REWARD', reward, 'DONE', done, "ENTROPY", entropy)
 
 cube_solver = tf.keras.Sequential([
-    tf.keras.layers.Flatten(),
-    DenseBN(units=4096, activation='relu'),
-    DenseBN(units=2048, activation='relu'),
-    DenseBN(units=2048, activation='relu'),
-    DenseBN(units=1024, activation='relu'),
+    #tf.keras.layers.Flatten(),
+    DenseBN(units=4096, activation='elu'),
+    DenseBN(units=2048, activation='elu'),
+    DenseBN(units=2048, activation='elu'),
+    DenseBN(units=1024, activation='elu'),
     tf.keras.layers.Dense(14),
 ])
 
 experiences_buffer = deque(maxlen=4000)
-discount = 0.99
+discount = 0.95
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
+residual_solver = tf.keras.Sequential([
+    ResidualBlock(4096,"elu"),
+    ResidualBlock(2048,"elu"),
+    ResidualBlock(2048,"elu"),
+    ResidualBlock(1024,"elu"),
+    tf.keras.layers.Dense(18)
+])
+
 #cube_solver=tf.keras.models.load_model('cubeRL.keras', custom_objects={'DenseBN':DenseBN})
-pre_training_loop(1000)
-training_loop(1000)
+training_loop(residual_solver, 1000)
+print("TESTING---------------")
+testing_loop(residual_solver, 1000)
